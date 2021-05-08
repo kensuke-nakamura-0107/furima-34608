559176b2dd264f1baca7d2a95f9d61ff041d2593
kensuke-nakamura-0107 <nakaken0107@yahoo.co.jp>
2021-05-08 18:10:17 +0900

    ユーザー管理_新規登録_バリデーション追加
    ユーザー管理_新規登録_バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,28 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  validates :nickname, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, {presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }}
+
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  validates :last_name_kanji , {presence: true, format: { with:  VALID_NAME_REGEX } }
+  validates :first_name_kanji, {presence: true, format: { with:  VALID_NAME_REGEX } }
+
+  VALID_KANA_REGEX = /\A[ァ-ヶー－]+\z/
+  validates :last_name_kana, {presence: true, format: { with:  VALID_KANA_REGEX } }
+  validates :first_name_kana, {presence: true, format: { with:  VALID_KANA_REGEX } }
+
+  validates :birthday , presence: true
+
+
+  #validates :nickname, presence: true, length: { maximum: 6 }
+
+  has_many :orders
+  has_many :items
+  has_many :comments
+  has_many :favorites
+  has_many :warnings
+
 end

3c1a352e7b970983f90e68d6df158075ce13a47f
kensuke-nakamura-0107 <nakaken0107@yahoo.co.jp>
2021-05-22 15:46:14 +0900

    商品出品機能_Active Hushの導入
    商品出品機能_Active Hushの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,9 +3,6 @@ class ItemsController < ApplicationController
   def index
   end
 
-  def new
-  end
-
   def create
   end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,8 +4,7 @@ class Item < ApplicationRecord
   validates :description, presence: true
   validates :category_id, presence: true
   validates :image, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
+  validates :condition_id, numericality: { other_than: 1 } 
   validates :postage_id, presence: true
   validates :prefecture_id, presence: true
   validates :period_id , presence: true
@@ -17,5 +16,11 @@ class Item < ApplicationRecord
   #has_many :comments
   #has_many :favorites
   #has_many :warnings
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :condition
+  belongs_to :category
+  belongs_to :postage
+  belongs_to :prefecture
 
 end

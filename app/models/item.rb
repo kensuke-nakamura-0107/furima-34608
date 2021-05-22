ab681d06f6737c785e0458eb25c56232f412ba57
kensuke-nakamura-0107 <nakaken0107@yahoo.co.jp>
2021-05-22 15:53:56 +0900

    商品出品機能_バリデーション設定（残分）
    商品出品機能_バリデーション設定（残分）

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,7 @@ class Item < ApplicationRecord
   validates :postage_id, numericality: { other_than: 1 } 
   validates :prefecture_id, numericality: { other_than: 1 } 
   validates :period_id , numericality: { other_than: 1 } 
-  validates :price, presence: true
+  validates :price,  presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
 
   belongs_to :user
   has_one_attached :image

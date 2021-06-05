59bb3d27a8926cc7ca431900d2962dc3bc7c5e86
kensuke-nakamura-0107 <nakaken0107@yahoo.co.jp>
2021-06-05 12:05:24 +0900

    商品一覧表示機能_新着順
    商品一覧表示機能_新着順

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,8 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :index]
 
   def index
-    @items = Item.all
+    query = "SELECT * FROM items ORDER BY id desc"
+    @items = Item.find_by_sql(query)
 
   end
 
@@ -27,6 +28,5 @@ def item_params
 params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :postage_id, :prefecture_id, :period_id, :price ).merge(user_id: current_user.id)
 
 end
-
 
 end

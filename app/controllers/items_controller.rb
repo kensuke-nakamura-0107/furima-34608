9be3ac3d6c7cbfd41475c32c5b4ff7764fb0638c
kensuke-nakamura-0107 <nakaken0107@yahoo.co.jp>
2021-06-09 21:36:21 +0900

    商品情報編集機能_フィードバック対応5
    商品情報編集機能_フィードバック対応5

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :set_item, only: [:edit, :update]
+  before_action :set_item, only: [:edit, :update, :show]
   before_action :user_identify, only: [:edit, :update]
 
   def index
@@ -8,7 +8,6 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def new
